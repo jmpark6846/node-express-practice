cc75b94a5f38b8d46b5f361187b195b55c4463e6
jmpark6846 <jmpark6846@gmail.com>
2018-04-16 14:18:30 +0900

    자잘한 수정 및 뷰 추가

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
+// load packages
 let express = require('express');
 let app = express();
-
+let bodyParser = require('body-parser');
 let handlebars = require('express-handlebars')
     .create({ defaultLayout: 'main'});
 
+// configure to use body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// static
 app.use(express.static(__dirname + '/public'));
+
+// template engine : handlebars
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+// port
 app.set('port', process.env.PORT || 3000);
+
+// TODO : routes
 
 app.get('/',function(req,res){
     res.render('home');
@@ -16,14 +26,15 @@ app.get('/',function(req,res){
 
 app.use(function(req, res){
     res.status(404);
-    res.send('404');
+    res.render('404');
 });
 
 app.use(function(err, req, res, next){
     console.error(err.stack);
     res.status(500);
-    res.send('500');
+    res.render('500');
 });
+
 
 app.listen(app.get('port'), function(){
     console.log('Express started on http://localhost:' + app.get('port'));

0ab86e84e971dd637f6c21c35425187fa38add31
jmpark6846 <jmpark6846@gmail.com>
2018-04-16 13:33:43 +0900

    jquery, bootstrap 설치. views, layouts 폴더 및 파일 추가

--- post.js
+++ post.js
@@ -4,11 +4,26 @@ let app = express();
 let handlebars = require('express-handlebars')
     .create({ defaultLayout: 'main'});
 
+app.use(express.static(__dirname + '/public'));
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+app.get('/',function(req,res){
+    res.render('home');
+});
+
+app.use(function(req, res){
+    res.status(404);
+    res.send('404');
+});
+
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500);
+    res.send('500');
+});
 
 app.listen(app.get('port'), function(){
     console.log('Express started on http://localhost:' + app.get('port'));
